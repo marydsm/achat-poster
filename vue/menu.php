938cc09aa0c5f08dfecbdf5dcc9b7559c833a5ad
Veyalokin <goumisch@gmail.com>
2017-03-30 17:04:50 +0200

    maj css

--- vue/menu.php
+++ vue/menu.php
@@ -1,5 +1,5 @@
 		<ul>
-			<li><a href="../control/page1.php" >Acceuil </a></li>
+			<li><a class='active' href="../control/page1.php" >Acceuil </a></li>
 			<li><a href="../control/utilisateurs_tab.php" >Gestion Clients </a></li>
 			<li><a href="../control/posters_tab.php">Gestion Posters </a></li>
 			<li><a href="../control/page2.php" >Contact </a></li>
