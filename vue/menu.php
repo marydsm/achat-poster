17bd22ab619935e0c86e3467dba5dda3f98dd062
Veyalokin <goumisch@gmail.com>
2017-03-31 19:25:15 +0200

    Nouveau type de Menu
    Dropdown ajouté.
    To Do: Corriger les défauts de couleur

--- vue/menu.php
+++ vue/menu.php
@@ -1,7 +1,18 @@
 		<ul>
-			<li id="home"><a class='active' href="../control/page1.php" >Acceuil </a></li>
-			<li id="customers"><a href="../control/utilisateurs_tab.php" >Gestion Clients </a></li>
-			<li id="products"><a href="../control/posters_tab.php">Gestion Posters </a></li>
-			<li id="contact"><a href="../control/page2.php" >Contact </a></li>
-			<li id="logout" class="logout"><a href="../control/destroy.php" >Logout </a></li>
+			<li id="home"><a class='active' href="../control/page1.php" >Acceuil</a></li>
+			<li id="customers" class="dropdown"><a class="dropbtn" href="javascript:void(0);" >Gestion Clients</a>
+				<div class="dropdown-content">
+					<a href="../control/utilisateurs_tab.php">Recherche Client</a>
+					<a href="#">Editer Client</a>
+				</div>
+			</li>	
+			<li id="products" class="dropdown"><a class="dropbtn" href="javascript:void(0);">Gestion Posters</a>
+				<div class="dropdown-content">
+					<a href="../control/posters_tab.php">Recherche Poster</a>
+					<a href="#">Editer Poster</a>
+					<a href="#">Ajouter Poster</a>
+				</div>
+			</li>
+			<li id="contact"><a href="../control/page2.php" >Contact</a></li>
+			<li id="logout"><a href="../control/destroy.php" >Logout</a></li>
 		</ul>
