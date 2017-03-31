dfc96707bec86bfdb9a2370754f26a1d24ca60dd
Veyalokin <goumisch@gmail.com>
2017-03-31 18:52:00 +0200

    31/03/2017

--- vue/menu.php
+++ vue/menu.php
@@ -1,7 +1,7 @@
 		<ul>
-			<li><a class='active' href="../control/page1.php" >Acceuil </a></li>
-			<li><a href="../control/utilisateurs_tab.php" >Gestion Clients </a></li>
-			<li><a href="../control/posters_tab.php">Gestion Posters </a></li>
-			<li><a href="../control/page2.php" >Contact </a></li>
+			<li class="home"><a class='active' href="../control/page1.php" >Acceuil </a></li>
+			<li class="customers"><a href="../control/utilisateurs_tab.php" >Gestion Clients </a></li>
+			<li class="products"><a href="../control/posters_tab.php">Gestion Posters </a></li>
+			<li class="contact"><a href="../control/page2.php" >Contact </a></li>
 			<li id="logout" class="logout"><a href="../control/destroy.php" >Logout </a></li>
 		</ul>
