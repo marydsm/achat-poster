29d5c49867eba63a2e2429192414a1bcb2a94bd4
Veyalokin <goumisch@gmail.com>
2017-03-31 20:51:39 +0200

    dfg

--- vue/js/RechAj.js
+++ vue/js/RechAj.js
@@ -2,8 +2,18 @@ var RechAj = function()
 {
 	$input=$(this);
 	$form=$input.parent("form");
-	alert($form.attr("action"));
-	alert($input.val());
+	$formAction = $form.attr("action");
+	$formValeur = $input.val();
+	$zoneResult = '#RESULT_'+$input.attr("name");
+	/*alert($form.attr("action"));
+	alert($input.val());*/
+
+	$formAction.replace('.php','_ajax.php');
+	/*alert($formAction);*/
+	$.post( $formAction, { RECH_AJAX : $formValeur}).done(function(data){
+		/*alert ("Data loaded" + data);*/
+		$($zoneResult).replaceWith(data);
+	});
 };
 
-$('.ecoute').on("keydown", RechAj);
+$('.ecoute').on("keyup", RechAj);
