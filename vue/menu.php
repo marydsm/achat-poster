6ad6ef28ec2fd6e10bf064a241adba822d31f58d
Veyalokin <goumisch@gmail.com>
2017-03-31 19:33:25 +0200

    Correction menu

--- vue/menu.php
+++ vue/menu.php
@@ -2,15 +2,15 @@
 			<li id="home"><a class='active' href="../control/page1.php" >Acceuil</a></li>
 			<li id="customers" class="dropdown"><a class="dropbtn" href="javascript:void(0);" >Gestion Clients</a>
 				<div class="dropdown-content">
-					<a href="../control/utilisateurs_tab.php">Recherche Client</a>
-					<a href="#">Editer Client</a>
+					<a class="submenu" href="../control/utilisateurs_tab.php">Recherche Client</a>
+					<a class="submenu" href="#">Editer Client</a>
 				</div>
 			</li>	
 			<li id="products" class="dropdown"><a class="dropbtn" href="javascript:void(0);">Gestion Posters</a>
 				<div class="dropdown-content">
-					<a href="../control/posters_tab.php">Recherche Poster</a>
-					<a href="#">Editer Poster</a>
-					<a href="#">Ajouter Poster</a>
+					<a class="submenu" href="../control/posters_tab.php">Recherche Poster</a>
+					<a class="submenu" href="#">Editer Poster</a>
+					<a class="submenu" href="#">Ajouter Poster</a>
 				</div>
 			</li>
 			<li id="contact"><a href="../control/page2.php" >Contact</a></li>
