6dde1fb72535eea555f83a9699025bf2c770b940
Veyalokin <goumisch@gmail.com>
2017-04-28 18:43:25 +0200

    incl. db

--- vue/menu.php
+++ vue/menu.php
@@ -1,18 +1,18 @@
 		<ul>
 			<li id="home"><a class='active' href="../control/page1.php" >Acceuil</a></li>
-			<li id="customers" class="dropdown"><a class="dropbtn" href="javascript:void(0);" >Gestion Clients</a>
+			<li id="customers" class="dropdown"><a id='inactive' class="dropbtn" href="javascript:void(0);" >Gestion Clients</a>
 				<div class="dropdown-content">
 					<a class="submenu" href="../control/utilisateurs_tab.php">Recherche Client</a>
 					<a class="submenu" href="../control/utilisateurs_edit.php">Editer Client</a>
 				</div>
 			</li>	
-			<li id="products" class="dropdown"><a class="dropbtn" href="javascript:void(0);">Gestion Posters</a>
+			<li id="products" class="dropdown"><a id='inactive' class="dropbtn" href="javascript:void(0);">Gestion Posters</a>
 				<div class="dropdown-content">
 					<a class="submenu" href="../control/posters_tab.php">Recherche Poster</a>
 					<a class="submenu" href="../control/posters_edit.php">Editer Poster</a>
 					<a class="submenu" href="../control/posters_add.php">Ajouter Poster</a>
 				</div>
 			</li>
-			<li id="contact"><a href="../control/page2.php" >Contact</a></li>
-			<li id="logout"><a href="../control/destroy.php" >Logout</a></li>
+			<li id="contact"><a id='inactive' href="../control/page2.php" >Contact</a></li>
+			<li id="logout"><a id='inactive' href="../control/destroy.php" >Logout</a></li>
 		</ul>
